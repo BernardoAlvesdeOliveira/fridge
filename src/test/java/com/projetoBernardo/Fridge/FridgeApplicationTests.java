f42c254740d3e2e323fb0bff2d5d54c4977eec69
BernardoAlvesdeOliveira <balvesdeoliveira3@gmail.com>
2025-11-20 17:50:20 -0300

    first commit

---
+++ src/test/java/com/projetoBernardo/Fridge/FridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoBernardo.Fridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
